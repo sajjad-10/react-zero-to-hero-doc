4c46a9518c17f26d49302f8c8c34b7d78eabd36b
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-01-02 21:44:07 +0330

    don jsx

--- JSX/src/Components/class/product.jsx
+++ JSX/src/Components/class/product.jsx
@@ -1,17 +1,35 @@
 import { Component } from "react";
 
 class Product extends Component {
-    state = {};
+    count = 0;
     render() {
+        const listElements = [
+            <li key="1">item 1</li>,
+            <li key="2">item 2</li>,
+            <li key="3">item 3</li>,
+        ];
+        const listItems = ["item 1", "item 2", "item 3"];
+        const mappedListItem = listItems.map((item, index) => (
+            <li key={index}>{item}</li>
+        ));
         return (
             <div>
                 <span className="m-2 text-info">Product Name</span>
-                <span className="m-2 badge bg-primary">2</span>
+                <span className="m-2 badge bg-primary">{this.format()}</span>
                 <button className="m-2 btn btn-sm btn-success">+</button>
                 <button className="m-2 btn btn-sm btn-warning">-</button>
                 <button className="m-2 btn btn-sm btn-danger">delete</button>
+                <ul>{listElements}</ul>
+                <ol>{mappedListItem}</ol>
             </div>
         );
+    }
+    format(count) {
+        if (this.count === 0) {
+            return "zero";
+        } else {
+            return this.count;
+        }
     }
 }
 
--- JSX/src/Components/func/product.jsx
+++ JSX/src/Components/func/product.jsx
@@ -1,11 +1,22 @@
 function product() {
+    const count = 2;
+    const imageUrl = "https://picsum.photos/100";
     return (
         <div>
             <span className="m-2 text-info">Product Name</span>
-            <span className="m-2 badge bg-primary">2</span>
+            <span className="m-2 badge bg-primary">
+                {count === 0 ? "zero" : count}
+            </span>
             <button className="m-2 btn btn-sm btn-success">+</button>
-            <button className="m-2 btn btn-sm btn-warning">-</button>
-            <button className="m-2 btn btn-sm btn-danger">delete</button>
+            {count !== 0 && (
+                <button className="m-2 btn btn-sm btn-warning">-</button>
+            )}
+            {count !== 0 ? (
+                <button className="m-2 btn btn-sm btn-danger">delete</button>
+            ) : (
+                ""
+            )}
+            <img src={imageUrl} alt="" style={{ borderRadius: "50%" }} />
         </div>
     );
 }
