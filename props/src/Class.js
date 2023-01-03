2a636b42fc6417f6f74ddf54d55966d2d5d71dd1
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-01-03 19:47:13 +0330

    intro props

---
+++ props/src/Class.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import Product from "./Components/class/product";
+class Class extends Component {
+    state = {};
+    render() {
+        return (
+            <>
+                <h3>React Class Components</h3>
+                <Product />
+            </>
+        );
+    }
+}
+
+export default Class;
---
+++ props/src/Components/class/product.jsx
@@ -0,0 +1,53 @@
+import { Component } from "react";
+
+class Product extends Component {
+    state = {
+        count: 2,
+    };
+    render() {
+        return (
+            <div>
+                <span className="m-2 text-info">Laptop</span>
+                <span className="m-2 badge bg-primary">{this.format()}</span>
+                <button
+                    onClick={this.handleIncrement}
+                    className="m-2 btn btn-sm btn-success"
+                >
+                    +
+                </button>
+                <button
+                    onClick={this.handleDecrement}
+                    className="m-2 btn btn-sm btn-warning"
+                >
+                    -
+                </button>
+                <button
+                    onClick={this.handleDelete}
+                    className="m-2 btn btn-sm btn-danger"
+                >
+                    delete
+                </button>
+            </div>
+        );
+    }
+    handleIncrement = () => {
+        const { count } = this.state;
+        this.setState({ count: count + 1 });
+    };
+    handleDecrement = () => {
+        const { count } = this.state;
+        this.setState({ count: count - 1 });
+    };
+    handleDelete = () => {
+        console.log("delete");
+    };
+    format() {
+        if (this.state.count === 0) {
+            return "zero";
+        } else {
+            return this.state.count;
+        }
+    }
+}
+
+export default Product;
