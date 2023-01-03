615d0d068799772e26742a756a90dce7eaae07e7
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-01-03 19:42:38 +0330

    update func state

--- state/src/Components/func/product.jsx
+++ state/src/Components/func/product.jsx
@@ -1,13 +1,35 @@
-function product() {
+import { useState } from "react";
+
+function Product() {
+    const [count, setCount] = useState(0);
+
     return (
         <div>
             <span className="m-2 text-info">Product Name</span>
-            <span className="m-2 badge bg-primary">2</span>
-            <button className="m-2 btn btn-sm btn-success">+</button>
-            <button className="m-2 btn btn-sm btn-warning">-</button>
+            <span className="m-2 badge bg-primary">{count}</span>
+            <button
+                onClick={handleIncrement}
+                className="m-2 btn btn-sm btn-success"
+            >
+                +
+            </button>
+            <button
+                onClick={() => {
+                    handleDecrement(count, setCount);
+                }}
+                className="m-2 btn btn-sm btn-warning"
+            >
+                -
+            </button>
             <button className="m-2 btn btn-sm btn-danger">delete</button>
         </div>
     );
+    function handleIncrement() {
+        setCount(count + 1);
+    }
+}
+function handleDecrement(count, setCount) {
+    setCount(count - 1);
 }
 
-export default product;
+export default Product;
