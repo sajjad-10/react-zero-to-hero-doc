921c4a436b7ef50f8424a1cd79eab04459ef200f
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-01-03 09:30:00 +0330

    don  event

--- events/src/Components/class/product.jsx
+++ events/src/Components/class/product.jsx
@@ -7,11 +7,37 @@ class Product extends Component {
             <div>
                 <span className="m-2 text-info">Product Name</span>
                 <span className="m-2 badge bg-primary">2</span>
-                <button className="m-2 btn btn-sm btn-success">+</button>
-                <button className="m-2 btn btn-sm btn-warning">-</button>
-                <button className="m-2 btn btn-sm btn-danger">delete</button>
+                <button
+                    onClick={this.handleIncrement.bind(this)}
+                    className="m-2 btn btn-sm btn-success"
+                >
+                    +
+                </button>
+                <button
+                    onClick={this.handleDecrement}
+                    className="m-2 btn btn-sm btn-warning"
+                >
+                    -
+                </button>
+                <button
+                    onClick={() => {
+                        this.handleDelete("Delete");
+                    }}
+                    className="m-2 btn btn-sm btn-danger"
+                >
+                    delete
+                </button>
             </div>
         );
+    }
+    handleIncrement() {
+        console.log("handleIncrement", this);
+    }
+    handleDecrement = () => {
+        console.log("handleDecrement", this);
+    };
+    handleDelete(text) {
+        console.log("handleDelete", text);
     }
 }
 
