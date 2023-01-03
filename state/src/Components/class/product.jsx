b3912795684a37337fbef5fd2384e04182ceadd7
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-01-03 19:10:21 +0330

    don event

--- state/src/Components/class/product.jsx
+++ state/src/Components/class/product.jsx
@@ -1,17 +1,47 @@
 import { Component } from "react";
 
 class Product extends Component {
-    state = {};
+    state = {
+        count: 2,
+        product: "Laptop",
+    };
     render() {
         return (
             <div>
-                <span className="m-2 text-info">Product Name</span>
-                <span className="m-2 badge bg-primary">2</span>
-                <button className="m-2 btn btn-sm btn-success">+</button>
-                <button className="m-2 btn btn-sm btn-warning">-</button>
+                <span className="m-2 text-info">{this.state.product}</span>
+                <span className="m-2 badge bg-primary">{this.format()}</span>
+                <button
+                    onClick={this.handleIncrement}
+                    className="m-2 btn btn-sm btn-success"
+                >
+                    +
+                </button>
+                <button
+                    onClick={this.handleDecrement}
+                    className="m-2 btn btn-sm btn-warning"
+                >
+                    -
+                </button>
                 <button className="m-2 btn btn-sm btn-danger">delete</button>
             </div>
         );
+    }
+    handleIncrement = () => {
+        // this.setState({ count: this.state.count + 1 }); this is slow
+
+        const count = this.state.count;
+        this.setState({ count: count + 1 });
+    };
+    handleDecrement = () => {
+        const { count } = this.state;
+        this.setState({ count: count - 1 });
+    };
+    format() {
+        if (this.state.count === 0) {
+            return "zero";
+        } else {
+            return this.state.count;
+        }
     }
 }
 
