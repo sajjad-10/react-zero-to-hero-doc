85269bf4398a1f9da4789044209c633e88331724
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-01-04 20:17:20 +0330

    change-parent-status inro

---
+++ change-parent-status/src/Components/class/products.jsx
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import Product from "./product";
+
+class Products extends Component {
+    state = {
+        products: [
+            { id: 1, count: 2, productName: "Laptop" },
+            { id: 2, count: 5, productName: "Phone" },
+            { id: 3, count: 3, productName: "AirPods" },
+        ],
+    };
+    render() {
+        return (
+            <div>
+                {this.state.products.map((product, index) => (
+                    <Product
+                        key={index}
+                        productName={product.productName}
+                        count={product.count}
+                    >
+                    </Product>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Products;
---
+++ change-parent-status/src/Components/func/products.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Product from "./product";
+
+const Products = () => {
+    const [products, setProducts] = useState([
+        { id: 1, count: 2, productName: "Laptop" },
+        { id: 2, count: 5, productName: "Phone" },
+        { id: 3, count: 3, productName: "AirPods" },
+    ]);
+    return (
+        <>
+            {products.map((product, index) => (
+                <Product
+                    key={index}
+                    productName={product.productName}
+                    count={product.count}
+                >
+                </Product>
+            ))}
+        </>
+    );
+};
+
+export default Products;
