ac79c75b52220496e26561f6b87b17b118287321
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-01-06 19:43:09 +0330

    React router DOM

---
+++ react-router-dom/src/class.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Users from "./components/class/users";
+class Class extends Component {
+    state = {};
+    render() {
+        return (
+            <>
+                <h3>React Class Components</h3>
+                <Users />
+            </>
+        );
+    }
+}
+
+export default Class;
