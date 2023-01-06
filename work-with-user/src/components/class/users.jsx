fa6c1352922b53dc8fe181c1d5cd68007ee7a739
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-01-06 12:52:45 +0330

    init Work with User

---
+++ work-with-user/src/components/class/users.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import axios from "axios";
+import LoadingUsers from "../loading/loadingUsers";
+
+class Users extends Component {
+    state = {
+        users: [], // on start is empty
+        isLoading: true,
+    };
+    async componentDidMount() {
+        const response = await axios.get("https://reqres.in/api/users/");
+        setTimeout(() => {
+            this.setState({ users: response.data.data, isLoading: false });
+        }, 3000);
+    }
+    render() {
+        return (
+            <>
+                <button
+                    onClick={this.handleCreate}
+                    className="btn btn-lg btn-primary"
+                >
+                    Create
+                </button>
+                <div className="row">
+                    {this.state.isLoading ? (
+                        <LoadingUsers />
+                    ) : (
+                        this.state.users.map((user, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className="col-4 text-center p-5"
+                                >
+                                    <img
+                                        src={user.avatar}
+                                        style={{
+                                            borderRadius: "50%",
+                                            width: "100px",
+                                        }}
+                                        alt={user.first_name}
+                                    />
+                                    <h4>
+                                        {user.first_name} {user.last_name}
+                                    </h4>
+                                    <h5>{user.email}</h5>
+                                    <div className="row">
+                                        <div className="col-6">
+                                            <button
+                                                onClick={this.handleUpdate}
+                                                className="btn btn-info btn-sm"
+                                            >
+                                                Update
+                                            </button>
+                                        </div>
+                                        <div className="col-6">
+                                            <button
+                                                onClick={this.handleDelete}
+                                                className="btn btn-danger btn-sm"
+                                            >
+                                                Delete
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    )}
+                </div>
+            </>
+        );
+    }
+    handleCreate = () => {};
+    handleUpdate = (user) => {};
+    handleDelete = (user) => {};
+}
+
+export default Users;
