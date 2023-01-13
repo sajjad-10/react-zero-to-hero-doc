3583218c7ab1b3a65792922ab4058f6826b86a2d
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-01-13 13:56:41 +0330

    update

--- users/src/components/class/login.jsx
+++ users/src/components/class/login.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 class Login extends Component {
-    state = {};
+    state = {
+        account: {
+            email: "",
+            password: "",
+        },
+    };
     render() {
+        const {email} = this.state.account
         return (
             <>
                 <form onSubmit={this.handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="email">Email:</label>
                         <input
+                            onChange={this.handleChange}
+                            value={email}
                             id="email"
+                            name="email"
                             className="form-control"
                             type="text"
                         />
@@ -16,7 +25,10 @@ class Login extends Component {
                     <div className="mb-3">
                         <label htmlFor="password">Password:</label>
                         <input
+                            onChange={this.handleChange}
+                            value={this.state.account.password}
                             id="password"
+                            name="password"
                             className="form-control"
                             type="text"
                         />
@@ -29,6 +41,12 @@ class Login extends Component {
     handleSubmit = (event) => {
         event.preventDefault();
         console.log("Submit");
+    };
+    handleChange = (event) => {
+        const input = event.currentTarget;
+        const accountNew = { ...this.state.account };
+        accountNew[input.name] = input.value;
+        this.setState({account : accountNew})
     };
 }
 
